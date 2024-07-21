e341e2b7a19b1ac2ab2b9453f4131f28fad04677
Muhammed Emir AYSAN <89260017+muhammedemiraysan@users.noreply.github.com>
2024-07-21 13:20:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-import select
-import sys
+import serial
 import time
-from machine import Pin
-# Set up the poll object
-led = Pin(25, Pin.OUT)
-poll_obj = select.poll()
-poll_obj.register(sys.stdin, select.POLLIN)
-# Loop indefinitely
-while True:
-    # Wait for input on stdin
-    poll_results = poll_obj.poll(1) # the '1' is how long it will wait for message before looping again (in microseconds)
-    if poll_results:
-        # Read the data from stdin (read data coming from PC)
-        data = sys.stdin.readline().strip()
-        # Write the data to the input file
-        if data == "1":
-            led.on()
-            print("ledon")
-        if data == "0":
-            led.off()
-            print("ledoff")
-    else:
-        # do something if no message received (like feed a watchdog timer)
-        continue
+from kumanda import JoystickController
+from motoryonsecim import ROVController
+# Seri port ayarları
+port = 'COM11'  # Windows için örnek. Linux/Mac için '/dev/ttyUSB0' veya '/dev/ttyACM0' olabilir.
+baudrate = 115200
+ser = serial.Serial(port, baudrate, timeout=1)
+kumanda = JoystickController()
+motor_speeds = [0] * 8  # 8 motor için hızlar
+def send_command(command):
+    ser.write((command + '\n').encode('utf-8'))  # Komutu gönder
+    response = ser.readline().decode('utf-8').strip()  # Cevabı oku
+    return response
+# İleri-geri, sağ-sol ve yukarı-aşağı kontrolü
+def update_motor_speeds(forward_backward, left_right, up_down, turret):
+    motor_speeds[0] = forward_backward - left_right  # sol ön motor
+    motor_speeds[1] = up_down # sol üst motor
+    motor_speeds[2] = forward_backward - left_right  # sol arka motor
+    motor_speeds[3] = forward_backward + left_right  # sağ ön motor
+    motor_speeds[4] = up_down  # sağ üst motor
+    motor_speeds[5] = forward_backward + left_right  # sağ arka motor
+    motor_speeds[6] = up_down
+    motor_speeds[7] = -up_down
+try:
+    while True:
+        command = str(kumanda.get_axes_values())
+        update_motor_speeds(kumanda.joystick.get_axis(1),kumanda.joystick.get_axis(0),kumanda.joystick.get_axis(2),kumanda.joystick.get_axis(3))
+        response = send_command(str(motor_speeds[0]) + "," + str(motor_speeds[1]) + "," + str(motor_speeds[2]) + "," + str(motor_speeds[3]) + "," + str(motor_speeds[4]) + "," + str(motor_speeds[5]) + str(motor_speeds[6]) + "," + str(motor_speeds[7]))
+        print("Pico'dan gelen cevap:", response)
+except KeyboardInterrupt:
+    print("Program durduruldu.")
+finally:
+    ser.close()
